3fdb7fc3a7c99a206d2044f9e0f72dda82f7238b
Dan McAulay <dmcaulay@gmail.com>
2013-08-02 11:51:13 -0700

    don't unzip the file

--- Library/Formula/expm.rb
+++ Library/Formula/expm.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Expm < Formula
   homepage 'http://expm.co/'
-  url 'http://expm.co/__download__/expm'
+  url 'http://expm.co/__download__/expm', :using => :nounzip
   sha1 'ddc367daa3d725900e065974d9e0b0fe40ef53e5'
   version '0.1'
 end
