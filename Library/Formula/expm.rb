e6ec88af1123e85523a12ec6c479ddb85abe1bc4
Dan McAulay <dmcaulay@gmail.com>
2013-08-02 11:40:18 -0700

    some dependencies were failing to build so im trying the executable

--- Library/Formula/expm.rb
+++ Library/Formula/expm.rb
@@ -2,19 +2,6 @@ require 'formula'
 
 class Expm < Formula
   homepage 'http://expm.co/'
-  url 'https://github.com/yrashk/expm/archive/0.1.tar.gz'
-  sha1 '3dcc3f081eb53543e297f03e7e64cdf653065720'
-
-  def install
-    system "make"
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test expm`.
-    system "false"
-  end
+  url 'http://expm.co/__download__/expm'
+  sha1 'ddc367daa3d725900e065974d9e0b0fe40ef53e5'
 end
